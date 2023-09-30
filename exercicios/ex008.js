dae8441ca883816c32615f84b94d55652a7f3e7b
Vitor Ryan da Rosa <vitorryanrosaaa@hotmail.com>
2023-09-29 22:45:55 -0300

    add exercicio na nova pasta

---
+++ exercicios/ex008.js
@@ -0,0 +1,7 @@
+var padrao = 60
+var vel = 40
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+    if(vel > 60) {
+        console.log(`Você ultrapassou a velocidade permitida de ${padrao}Km/h, MULTADO! `)
+    }
+console.log(`Dirija com cuidado, use o cinto de segunraça!!`)
---
+++ exercicios/ex009.js
@@ -0,0 +1,6 @@
+var país = 'Brasil'
+if(país == 'Brasil') {
+    console.log('Você é Brasileiro!')
+} else {
+    console.log('Você é americano')
+}
